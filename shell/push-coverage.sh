a16f33ac4ceeccf0ac1bc7ef99158d731fe6e773
Nik Boyd <nikboyd@sonic.net>
2021-12-03 17:12:48 -0800

    force shallow copy

--- shell/push-coverage.sh
+++ shell/push-coverage.sh
@@ -10,6 +10,7 @@ for lib_name in $lib_folders; do
     cover_folder="$lib_name/target/site/jacoco"
     cover_file="$lib_name/target/site/jacoco/index.html"
 
+    rm -R docs/$lib_name/
     cp -R $cover_folder docs/$lib_name/
 done
 
