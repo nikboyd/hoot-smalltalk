0762944b9c62a003c227aade1118aae13076279c
Nik Boyd <nikboyd@sonic.net>
2021-12-03 16:46:48 -0800

    fix copy

--- shell/push-coverage.sh
+++ shell/push-coverage.sh
@@ -7,12 +7,10 @@ if [[ $mvn_code == 0 ]]; then
 
 lib_folders="hoot-runtime hoot-compiler hoot-maven-plugin libs-hoot"
 for lib_name in $lib_folders; do
-    if [[ ! -d docs/$lib_name ]]; then mkdir docs/$lib_name; fi
-
     cover_folder="$lib_name/target/site/jacoco"
     cover_file="$lib_name/target/site/jacoco/index.html"
 
-    cp $cover_folder docs/$lib_name/
+    cp -R $cover_folder docs/$lib_name/
 done
 
 fi
