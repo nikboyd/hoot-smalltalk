eb3a1f49b43eb55d5a3cd69d480310b1478c75a6
Nik Boyd <nikboyd@sonic.net>
2023-11-25 13:32:47 -0600

    move docs image to docs folder

--- shell/build-docs-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build-docs-image.sh build a docker container image to host hoot-docs-bundle
-
-./remove-all-runners.sh
-docker rmi hoot-docs-host
-
-mkdir -p jars
-cp ../hoot-docs-bundle/target/*.jar jars/
-docker build -t hoot-docs-host -f hoot-docs-dockerfile .
