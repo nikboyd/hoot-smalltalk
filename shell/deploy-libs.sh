dbd34854c0b01eca7ffc90ea46e3f6d9bb3b061f
Nik Boyd <nikboyd@sonic.net>
2023-12-02 13:12:42 -0600

    add docs bundle to build

--- shell/deploy-libs.sh
+++ shell/deploy-libs.sh
@@ -11,7 +11,7 @@ fi
 
 # publish built JARs if build passed
 if [[ $mvn_code == 0 ]]; then
-    hoot_libs="hoot-compiler-boot hoot-maven-plugin hoot-compiler-bundle hoot-libs-bundle"
+    hoot_libs="hoot-compiler-boot hoot-maven-plugin hoot-compiler-bundle hoot-libs-bundle hoot-docs-bundle"
     for lib_name in $hoot_libs; do
         shell/deploy-lib.sh "$lib_name" jar $version
     done
