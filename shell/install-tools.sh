7f042ebb1a67d12bcccc6b96a0c17fbd7c47ad8a
Nik Boyd <nikboyd@sonic.net>
2023-12-06 08:35:06 -0600

    no sudo [skip ci]

--- shell/install-tools.sh
+++ shell/install-tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # installs common tools
 
-sudo apt-get update
-sudo apt-get install software-properties-common
+apt-get update
+apt-get install software-properties-common
 
 #sudo apt-get update
 #sudo apt-get install -y maven
