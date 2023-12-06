cad384cefccff2a9e80068926e1d62514fe1f0e0
Nik Boyd <nikboyd@sonic.net>
2023-12-06 08:55:38 -0600

    install pip [skip ci]

--- shell/install-tools.sh
+++ shell/install-tools.sh
@@ -3,9 +3,9 @@
 
 apt-get update
 apt-get install software-properties-common
+apt-get install -y python3-pip
 python3 --version
-python3 -m pip --version
-#pip --version
+pip --version
 
 #sudo apt-get update
 #sudo apt-get install -y maven
