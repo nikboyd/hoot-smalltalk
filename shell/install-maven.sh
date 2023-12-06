db2b6b8ec71884352375dbda24bc6a0e7189c2f9
Nik Boyd <nikboyd@sonic.net>
2023-12-06 06:26:19 -0600

    separate installs

---
+++ shell/install-maven.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# installs jdk + maven
+
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk-headless
+
+sudo apt-get update
+sudo apt-get install -y maven
+
--- shell/install-tools.sh
+++ shell/install-tools.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# installs jdk 8 + maven
+# installs common tools
 
 sudo apt-get update
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get install -y openjdk-8-jdk-headless
 
-sudo apt-get update
-sudo apt-get install -y maven
+#sudo apt-get update
+#sudo apt-get install -y maven
