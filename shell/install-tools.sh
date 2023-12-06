982bf3aaab01d91c76b1277b0b6e15fa8774430d
Nik Boyd <nikboyd@sonic.net>
2023-12-06 08:51:16 -0600

    check pip [skip ci]

--- shell/install-tools.sh
+++ shell/install-tools.sh
@@ -4,8 +4,8 @@
 apt-get update
 apt-get install software-properties-common
 python3 --version
-python3 -m ensurepip --default-pip
-pip --version
+python3 -m pip --version
+#pip --version
 
 #sudo apt-get update
 #sudo apt-get install -y maven
