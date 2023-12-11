3e58432ec1a5f1728304d67ca90f6d757a72822a
Nik Boyd <nikboyd@sonic.net>
2023-12-11 07:24:41 -0600

    fix maven opt [skip ci]

--- shell/pull-lib.sh
+++ shell/pull-lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pull-lib.sh ... pull JAR from cloudsmith package repository, $1=library name, $2=library type, $3=library version
+# pull-lib.sh ... pull JAR from github package repository, $1=library name, $2=library type, $3=library version
 
 get_deps='org.apache.maven.plugins:maven-dependency-plugin:2.8:get'
 location='central::default::github'
@@ -13,25 +13,27 @@ lib_vers=''
 if [ $3 ]; then lib_vers="$3"; fi
 
 mvn_opts='-U -B'
-if [ -d /workspace ]; then
-   maven_opts+=" --settings .m2/lib-settings.xml"
-   lib_vers=$( cat $1-version.txt )
+if [[ $USER == runner ]]; then # cloud build
+   mvn_opts+=" --settings .m2/lib-settings.xml"
+   if [ ! $3 ]; then lib_vers=$( cat $1-version.txt ); fi
 else # fetch lib version locally
    if [ ! $3 ]; then lib_vers=$( shell/query-repo.sh $1 ); fi
 fi
 
-echo "pulling $lib_name:$lib_vers:$lib_type"
+echo "pulling $lib_group:$lib_name:$lib_vers:$lib_type"
 lib_pom="$lib_name-$lib_vers.pom.xml"
 lib_file="$lib_name-$lib_vers.$lib_type"
 lib_folder="$HOME/.m2/repository/$lib_group/$lib_name/$lib_vers"
 
 # pull the JAR
-if [ ! $2 ]; then
+if [ ! -f $lib_folder/$lib_file ]; then
+echo "$USER pulling from $location opts $mvn_opts"
 mvn $mvn_opts $get_deps -DremoteRepositories=$location -Dtransitive=false \
     -DgroupId=$lib_group -DartifactId=$lib_name -Dversion=$lib_vers -Dpackaging=$lib_type
 fi
 
 if [ ! -f $lib_folder/$lib_pom ]; then
+echo "$USER pulling from $location opts $mvn_opts"
 mvn $mvn_opts $get_deps -DremoteRepositories=$location -Dtransitive=false \
     -DgroupId=$lib_group -DartifactId=$lib_name -Dversion=$lib_vers -Dpackaging=pom
 fi
