5d9b9228176380cfce8cc43f90d13d219049d11e
Nik Boyd <nikboyd@sonic.net>
2023-12-12 17:08:34 -0600

    set local repo [skip ci]

--- shell/pull-lib.sh
+++ shell/pull-lib.sh
@@ -13,8 +13,10 @@ lib_vers=''
 if [ $3 ]; then lib_vers="$3"; fi
 
 mvn_opts='-U -B'
+local_repo=''
 if [ -d /workspace ]; then # cloud build
    mvn_opts+=" --settings .m2/lib-settings.xml"
+   local_repo="-Dmaven.repo.local=/workspace/.m2/repository"
    if [ ! $3 ]; then lib_vers=$( cat $1-version.txt ); fi
 else # fetch lib version locally
    if [ ! $3 ]; then lib_vers=$( shell/query-repo.sh $1 ); fi
@@ -27,13 +29,13 @@ lib_folder="$HOME/.m2/repository/$lib_group/$lib_name/$lib_vers"
 
 # pull the JAR
 if [ ! -f $lib_folder/$lib_file ]; then
-echo "$USER pulling from $location opts $mvn_opts"
-mvn $mvn_opts $get_deps -DremoteRepositories=$location -Dtransitive=false \
+echo "$USER pulling from $location opts $mvn_opts $local_repo"
+mvn $mvn_opts $get_deps $local_repo -DremoteRepositories=$location -Dtransitive=false \
     -DgroupId=$lib_group -DartifactId=$lib_name -Dversion=$lib_vers -Dpackaging=$lib_type
 fi
 
 if [ ! -f $lib_folder/$lib_pom ]; then
-echo "$USER pulling from $location opts $mvn_opts"
-mvn $mvn_opts $get_deps -DremoteRepositories=$location -Dtransitive=false \
+echo "$USER pulling from $location opts $mvn_opts $local_repo"
+mvn $mvn_opts $get_deps $local_repo -DremoteRepositories=$location -Dtransitive=false \
     -DgroupId=$lib_group -DartifactId=$lib_name -Dversion=$lib_vers -Dpackaging=pom
 fi
