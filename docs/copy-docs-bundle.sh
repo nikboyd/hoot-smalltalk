f5e31f5a1ff13dbda45182e655b2be2ee76d360e
Nik Boyd <nikboyd@sonic.net>
2023-12-12 07:56:27 -0600

    update docs recipe [skip ci]

--- docs/copy-docs-bundle.sh
+++ docs/copy-docs-bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# fetch-docs-bundle.sh download docs bundle and copy to jars folder
+# copy-docs-bundle.sh copy to docs/jars
 
 rm -fr docs/jars
 mkdir -p docs/jars
@@ -7,6 +7,5 @@ docs_loc=".m2/repository/hoot-smalltalk"
 docs_lib=hoot-docs-bundle
 docs_version=$1
 docs_file="$docs_lib-$docs_version.jar"
-shell/pull-lib.sh $docs_lib # fetch from remote repo to local
 cp ~/$docs_loc/$docs_lib/$docs_version/$docs_file docs/jars/
 ls -al docs/jars/
