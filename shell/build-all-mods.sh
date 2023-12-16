b2fc14aacc864e00ef6888783833670eed3f3b62
Nik Boyd <nikboyd@sonic.net>
2023-12-16 07:02:10 -0600

    use DOCS_TAG to deploy container [skip ci]

--- shell/build-all-mods.sh
+++ shell/build-all-mods.sh
@@ -28,20 +28,6 @@ if [[ $USER == runner ]]; then
     # push bundles to package registry
     shell/deploy-libs.sh $mvn_code $stamp
 
-    # clear build changes from file system
-    #git stash -u
-
-    # build badges after tests
-    #shell/build-mvn-badge.sh $mvn_code $stamp
-    #shell/build-cover-badges.sh $mvn_code
-
-    # copy coverage reports
-    #shell/copy-coverage.sh $mvn_code
-    #ls docs/hoot-runtime/
-
-    # push test coverage reports
-    #shell/push-coverage.sh $mvn_code
-
     # build and push version tag
     shell/build-git-tag.sh $mvn_code $stamp
 
