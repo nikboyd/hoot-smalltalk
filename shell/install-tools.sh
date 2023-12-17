1572ad26858b2586e2833c38a63d1395e2a79a04
Nik Boyd <nikboyd@sonic.net>
2023-12-17 14:53:42 -0600

    revert [skip ci]

--- shell/install-tools.sh
+++ shell/install-tools.sh
@@ -6,8 +6,3 @@ apt-get install software-properties-common
 apt-get install -y python3-pip
 python3 --version
 pip --version
-
-if [ -d /workspace ]; then
-    BUNDLE_TAG=$( git describe --abbrev=0 )
-    echo "$BUNDLE_TAG" > /workspace/hoot-docs-bundle-version.txt
-fi
